e87381862f0c1df2c8be161548963c020b66150f
gsunil1996 <gajulasunilkumar123@gmail.com>
2023-07-12 23:21:26 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const cookieParser = require("cookie-parser");
 const cloudinary = require("cloudinary");
 const fileUpload = require('express-fileupload')
 
-// import all routes
 const productRoutes = require("./routes/productRoutes");
 const authRoutes = require("./routes/authRoutes");
 const orderRoutes = require("./routes/orderRoutes");
@@ -14,43 +13,30 @@ const cartRoutes = require('./routes/cartRoutes');
 const paymentRoutes = require("./routes/paymentRoutes");
 
 require("dotenv").config();
-
-// Handle Uncaught exceptions
-process.on("uncaughtException", (err) => {
-  console.log(`ERROR: ${err.stack}`);
-  console.log("Shutting down due to uncaught exception");
-  process.exit(1);
-});
-
-
 require("./db/database");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload());
+
 app.use(cors({
-  // origin: 'http://localhost:3000',
   origin: 'https://shop-now-e-commerce.vercel.app',
   credentials: true
 }));
 
 app.options('*', cors());
 
-app.use(cookieParser());
-app.use(fileUpload());
-
-// routes
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", orderRoutes);
 app.use('/api/v1', cartRoutes);
 app.use('/api/v1', paymentRoutes);
 
-// middleware to handle errors
 app.use(errorMiddleware);
 
-// Setting up cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -61,7 +47,6 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on PORT: ${process.env.PORT}`);
 });
 
-// Handle Unhandled Promise rejections
 process.on("unhandledRejection", (err) => {
   console.log(`ERROR: ${err.stack}`);
   console.log("Shutting down the server due to Unhandled Promise rejection");
