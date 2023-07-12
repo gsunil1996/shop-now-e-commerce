a3f686dc132ddfa6eb040e1b893b513adf1f62cb
gsunil1996 <gajulasunilkumar123@gmail.com>
2023-07-12 23:28:21 +0530

    url corrected

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const cloudinary = require("cloudinary");
 const fileUpload = require('express-fileupload')
 
+// import all routes
 const productRoutes = require("./routes/productRoutes");
 const authRoutes = require("./routes/authRoutes");
 const orderRoutes = require("./routes/orderRoutes");
@@ -13,33 +14,43 @@ const cartRoutes = require('./routes/cartRoutes');
 const paymentRoutes = require("./routes/paymentRoutes");
 
 require("dotenv").config();
+
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
+
+
 require("./db/database");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
-app.use(fileUpload());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://shop-now-e-commerce.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors({
+  // origin: 'http://localhost:3000',
+  origin: 'https://shop-now-e-commerce-eb1l.vercel.app/',
+  credentials: true
+}));
 
 app.options('*', cors());
 
+app.use(cookieParser());
+app.use(fileUpload());
+
+// routes
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", orderRoutes);
 app.use('/api/v1', cartRoutes);
 app.use('/api/v1', paymentRoutes);
 
+// middleware to handle errors
 app.use(errorMiddleware);
 
+// Setting up cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -50,6 +61,7 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on PORT: ${process.env.PORT}`);
 });
 
+// Handle Unhandled Promise rejections
 process.on("unhandledRejection", (err) => {
   console.log(`ERROR: ${err.stack}`);
   console.log("Shutting down the server due to Unhandled Promise rejection");
